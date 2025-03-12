e45318506ade91ff20e457f144d3ed238b2755c1
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-12 16:53:40 +0700

    menambahkan class FanLaptop

--- Program.cs
+++ Program.cs
@@ -37,3 +37,55 @@ class KodeProduk
         Console.WriteLine($"kode produk untuk {produk} adalah {KodeProduk}");
     }
 }
+
+public class Fan
+{
+    enum State { QUIET, BALANCED, PERFORMANCE, TURBO };
+    public static void Main()
+    {
+        State state = State.QUIET;
+        string command = "";
+        string[] screenName = { "QUIET", "BALANCED", "PERFORMANCE", "TURBO" };
+        while (command != "EXIT")
+        {
+            Console.WriteLine(" ");
+            Console.WriteLine(screenName[(int)state] + " SCREEN");
+            Console.Write("Enter Command : ");
+
+            command = Console.ReadLine();
+            switch (state)
+            {
+                case State.QUIET:
+                    if (command == "mode up quiet")
+                        state = State.BALANCED;
+                    else if (command == "turbo shortcut quiet")
+                        state = State.TURBO;
+                        Console.WriteLine("Fan Quiet Beubah Menjadi Turbo");
+                    break;
+
+                case State.BALANCED:
+                    if (command == "mode down balanced")
+                        state = State.QUIET;
+                    else if (command == "mode up balanced")
+                        state = State.PERFORMANCE;
+                        Console.WriteLine("Fan Quiet berubah menjadi Balance");
+                    break;
+
+                case State.PERFORMANCE:
+                    if (command == "mode down performance")
+                        state = State.BALANCED;
+                    else if (command == "mode up performance")
+                        state = State.TURBO;
+                    break;
+
+                case State.TURBO:
+                    if (command == "mode down turbo")
+                        state = State.PERFORMANCE;
+                    else if (command == "turbo shortcut turbo")
+                        state = State.QUIET;
+                    break;
+            }
+        }
+        Console.WriteLine("EXIT SCREEN");
+    }
+}
